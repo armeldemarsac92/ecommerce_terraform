637ea0d7da4e21900dd2e38f2c93ae1c936139fa
Armel De Marsac <armeldemarsac@gmail.com>
2025-03-03 12:25:14 +0100

    fix : updated the tf state key

--- environments/production/backend.tf
+++ environments/production/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tdev700-terraform-state"
-    key            = "tdev700/prod.tfstate"
+    key            = "tdev700/production.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
